250128f24af42d2325259a0400ced975d6e22228
Illia Katerynych <iliakat.kr@gmail.com>
2024-08-06 12:07:05 +0100

    scheduling added

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 from flask import Flask, render_template, jsonify
-from mathsapi import get_json
+from mathsapi import get_result
 import json
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template("index.html", data=get_json())
+    return render_template("index.html", data=get_result())
--- mathsapi/__init__.py
+++ mathsapi/__init__.py
@@ -2,6 +2,10 @@ import mathsapi.quadratic as quadratic
 import mathsapi.polynomials as polynomials
 import mathsapi.pythagoras as pythagoras
 
+import schedule
+import time
+import threading
+
 def get_json():
   roots, coeff, poly = quadratic.generate_polynomial()
   
@@ -39,3 +43,30 @@ def get_json():
   }
   
   return questions
+
+result = None
+
+def get_result():
+  global result
+  return result
+
+def update_json():
+  global result
+  result = get_json()
+  print("Updated JSON")
+
+update_json()
+schedule.every().day.at("00:00").do(update_json)
+
+def run_scheduler():
+  while True:
+    schedule.run_pending()
+    time.sleep(1)
+
+scheduler_thread = threading.Thread(target=run_scheduler)
+scheduler_thread.daemon = True
+scheduler_thread.start()
+
+if __name__ == "__main__":
+  while True:
+    time.sleep(10)
